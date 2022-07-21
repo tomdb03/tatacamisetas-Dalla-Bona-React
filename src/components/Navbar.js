f8c476fa571c004a6572bde213508520da7ba239
tomdb03 <tomasdallabona@gmail.com>
2022-07-21 11:18:22 -0300

    agrego sass y creo navbar, primer componente

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <nav className="Navbar">
+            <div>
+                <h1>Tata Camisetas</h1>
+            </div>
+            <div>
+                <button>Camisetas</button>
+                <button>Shorts</button>
+                <button>Buzos</button>
+                <button>Joggins</button>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
