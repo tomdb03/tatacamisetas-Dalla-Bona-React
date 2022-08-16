8938fc78505e50deaadeb8d13ae205ebade05072
tomdb03 <tomasdallabona@gmail.com>
2022-08-16 12:35:23 -0300

    estilos cart view

--- src/components/CartDiv.js
+++ src/components/CartDiv.js
@@ -11,27 +11,27 @@ const CartDiv = () => {
 
     return (
         <div className='carrito'>
-            <h1 className='sectionTitle'>Detalle</h1>
-            <div className='prodIndividual'>
-                <div className='divDescripcion'>Producto</div>
-                <div>Cantidad</div>
-                <div className='divCash'>Precio</div>
-                <div className='divCash'>Subtotal</div>
-                <div></div>
+            <h1 className='sectionTitle'>Carrito</h1>
+            <div className='prodIndividual titulos'>
+                <div className='prodName'>Producto</div>
+                <div className='noName'>Cantidad</div>
+                <div className='noName'>Precio</div>
+                <div className='noName'>Subtotal</div>
+                <div className='noName'></div>
             </div>
             {prodInCart === 0 && <div className='carroVacio'>No hay nada en el carrito</div>}
             {cart.map(prod => <div key={prod.id} className="prodIndividual">
-                <div className='divDescripcion'>{prod.name}</div>
-                <div>{prod.quantity} {prod.quantity > 1 ? 'unidades' : 'unidad'}</div>
-                <div className='divCash'>${prod.price}</div>
-                <div className='divCash'>${prod.price * prod.quantity}</div>
-                <button onClick={() => removeItem(prod.id)}>remover</button>
+                <div className='prodName'>{prod.name}</div>
+                <div className='noName'>{prod.quantity} {prod.quantity > 1 ? 'unidades' : 'unidad'}</div>
+                <div className='noName'>${prod.price}</div>
+                <div className='noName'>${prod.price * prod.quantity}</div>
+                <div className='noName'><button className='remover' onClick={() => removeItem(prod.id)}>remover</button></div>
             </div>)}
             <div className='pieCarrito'>
-                {prodInCart > 0 ? <button onClick={() => clearCart()}>Vaciar carrito</button> : <Link to="/">Volver al catálogo</Link>}
+                {prodInCart > 0 ? <button className="btncart vaciar" onClick={() => clearCart()}>Vaciar carrito</button> : <Link className="btncart volver" to="/">Volver al catálogo</Link>}
                 {prodInCart > 0 ? <div> Cantidad total de productos: {prodInCart}</div> : null}
                 {prodInCart > 0 ? <div> Total de compra: ${compraTotal}</div> : null}
-                {prodInCart > 0 ? <button>Terminar compra</button> : null}
+                {prodInCart > 0 ? <button className="btncart terminar">Terminar compra</button> : null}
             </div>
         </div>
     )
